ead9da77630cf7d4caa7d0b172087179d0bba1b1
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2024-05-19 21:16:59 +0800

    change font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,29 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: '最终幻想14英文笔记',
-			social: {
-				github: 'https://github.com/Kaida-Amethyst/ffxiv_notes',
-			},
-			sidebar: [
-				{
-					label: 'Introduction',
-					items: [
-						{ label: 'Introduction', link: '/introduction/introduction/' },
-					],
-				},
-				{
-					label: 'HeavenSword',
-					autogenerate: { directory: 'HeavenSword' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: '最终幻想14英文笔记',
+    customCss: [
+      './src/tailwind.css',
+    ],
+    social: {
+      github: 'https://github.com/Kaida-Amethyst/ffxiv_notes'
+    },
+    sidebar: [{
+      label: 'Introduction',
+      items: [{
+        label: 'Introduction',
+        link: '/introduction/introduction/'
+      }]
+    }, {
+      label: 'HeavenSword',
+      autogenerate: {
+        directory: 'HeavenSword'
+      }
+    }]
+  }), tailwind()]
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+import starlightPlugin from '@astrojs/starlight-tailwind';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['"CaskaydiaMonoNerd"'],
+				mono: ['"CaskaydiaMonoNerd"'],
+			},
+		},
+	},
+	plugins: [starlightPlugin()],
+}
